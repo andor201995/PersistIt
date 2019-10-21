ef0e5ff20501b90f5b4d9e61a42beaff5b51a11e
Anmol Srivastava <andor201995@users.noreply.github.com>
2019-10-21 19:16:11 +0530

    back press case solved

--- app/src/main/java/com/andor/bottomsheetlockit/MainActivity.kt
+++ app/src/main/java/com/andor/bottomsheetlockit/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andor.bottomsheetlockit
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,12 +21,30 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         bottomSheetBehavior =
             BottomSheetBehavior.from(findViewById<View>(R.id.bottomSheetNavHostFragment))
+        bottomSheetBehavior.setBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(p0: View, p1: Float) {
+            }
+            override fun onStateChanged(p0: View, p1: Int) {
+                when (bottomSheetBehavior.state) {
+                    BottomSheetBehavior.STATE_DRAGGING -> bottomSheetBehavior.state =
+                        BottomSheetBehavior.STATE_EXPANDED
+                }
+            }
+        })
+
         button_open_bottom_sheet.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
         }
     }
 
     override fun onBackPressed() {
+        val navController =
+            Navigation.findNavController(findViewById(R.id.bottomSheetNavHostFragment))
+        if (navController.currentDestination!!.id == R.id.bottomMenuFragment && bottomSheetBehavior.state != BottomSheetBehavior.STATE_HIDDEN) {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            return
+        }
         super.onBackPressed()
     }
 }
